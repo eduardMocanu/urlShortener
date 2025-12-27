97b7ec14d5cbbe90cee16fc57db93faf8c77e967
Eduard Mocanu <mocanueduardgabi@gmail.com>
2025-12-27 16:56:33 +0200

    added the OAuth2 + password/username login + security configurations done to do: implement the JWT service + filter

--- src/main/java/com/example/urlShortenerServer/service/MyOAuth2UserService.java
+++ src/main/java/com/example/urlShortenerServer/service/MyOAuth2UserService.java
@@ -8,7 +8,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-
+//
 
 import java.util.Map;
 @Service
